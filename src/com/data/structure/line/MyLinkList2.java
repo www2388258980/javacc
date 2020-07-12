22d8b715519f02645cc83167febc4f9771a3daf7
www2388258980 <2388258980@qq.com>
2020-07-12 10:03:56 +0800

    单链表练习题

--- src/com/data/structure/line/MyLinkList2.java
+++ src/com/data/structure/line/MyLinkList2.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /*
  * 单链表
  */
-public class MyLinkList2<T> implements Serializable {
+public class MyLinkList2<T extends Comparable> implements Serializable {
 
     // 头节点;
     private Node<T> headNode;
@@ -24,16 +24,6 @@ public class MyLinkList2<T> implements Serializable {
             this.next = next;
         }
 
-        public Node<T> next() {
-            if (this.next == null) {
-                return null;
-            }
-            return this.next;
-        }
-
-        public T data() {
-            return this.data;
-        }
 
         public T getData() {
             return data;
@@ -93,12 +83,53 @@ public class MyLinkList2<T> implements Serializable {
         System.out.print("[");
         while (p != null) {
             System.out.print(p.data);
-            p = p.next();
+            p = p.next;
             if (p != null) {
                 System.out.print(",");
             }
         }
         System.out.println("]");
+    }
+
+    /**
+     * 删除递增有序链表中值大于mink且小于maxk的所有元素
+     * <p>
+     * 思路：
+     * ***遍历递增链表,找到最后一个元素中的值小于等于mink，将下一个元素赋给p，找到最后一个元素中的值小于maxk，将其赋给q
+     * ***那么p和q([p,q])中间的元素节点就满足条件
+     * 时间复杂度:
+     * ***O(n)  --- n为链表的长度
+     * 空间复杂度
+     * ***O(1)
+     *
+     * @param mink
+     * @param maxk
+     */
+    public void removeSection(T mink, T maxk) {
+        if (mink.compareTo(maxk) >= 0) {
+            return;
+        }
+        Node<T> t = this.headNode.next;
+        Node<T> p = null;
+        Node<T> q = null;
+        while (t != null) {
+            T data = t.data;
+            if (data.compareTo(mink) <= 0) {
+                p = t.next;
+            }
+            if (data.compareTo(maxk) < 0) {
+                q = t;
+            }
+            t = t.next;
+
+        }
+
+        if (p == null) { // 代表链表中的元素都大于mink
+            q.next = null;
+        } else {
+            this.headNode.next = p;
+            q.next = null;
+        }
     }
 
 
