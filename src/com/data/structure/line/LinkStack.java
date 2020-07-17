c607f43a26bdcaffe29fadc438357d54f4b51900
www2388258980 <2388258980@qq.com>
2020-07-17 22:57:21 +0800

    顺序栈和链栈的实现

---
+++ src/com/data/structure/line/LinkStack.java
@@ -0,0 +1,64 @@
+package com.data.structure.line;
+
+/**
+ * 链栈
+ *
+ * @param <T>
+ */
+public class LinkStack<T> {
+
+    private StackNode<T> top;
+
+    public LinkStack() {
+
+    }
+
+
+    private static class StackNode<T> {
+        private T data;
+        private StackNode next;
+
+        public StackNode(T data) {
+            this(data, null);
+
+        }
+
+        public StackNode(T data, StackNode<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public void push(T arg1) {
+        StackNode<T> p = new StackNode<>(arg1, this.top);
+        this.top = p;
+    }
+
+    public T pop() {
+        if (this.top == null) {
+            return null;
+        }
+        StackNode<T> p = this.top;
+        this.top = this.top.next;
+        return p.data;
+    }
+
+    public T getTop() {
+        return this.top == null ? null : this.top.data;
+    }
+
+    @Override
+    public String toString() {
+        System.out.print("[");
+        StackNode<T> p = this.top;
+        while (p != null) {
+            System.out.print(p.data.toString());
+            if (p.next != null) {
+                System.out.print(",");
+            }
+            p = p.next;
+        }
+        System.out.println("]");
+        return super.toString();
+    }
+}
