f4f725767c63089ac4d7b41e31f3f34e8090c9db
www2388258980 <2388258980@qq.com>
2021-05-17 10:22:29 +0800

    test- init

--- src/com/yj/t_2020_12/Test_120101.java
+++
@@ -1,8 +0,0 @@
-package com.yj.t_2020_12;
-
-public class Test_120101 {
-		public static void main(String[] args) {
-				String disableCacheName = String.format("fs.%s.impl.disable.cache", "hdfs");
-				System.out.println(disableCacheName);
-		}
-}
